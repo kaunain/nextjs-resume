628ce3ac99297c49b2e766fbaea520ab5e0cccf1
Kaunain Ahmad <kau9.ahmad@gmail.com>
2026-07-02 10:14:25 +0530

    Add Cloudflare Pages deployment support

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,8 @@
+import { initOpenNextCloudflareForDev } from '@opennextjs/cloudflare';
 import { NextConfig } from 'next';
 import { withContentlayer } from 'next-contentlayer2';
+
+initOpenNextCloudflareForDev();
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
